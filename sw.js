cc6f51c7c4941e83f2a2661909d4d1dcc7c83ea2
Erick Cares <saturnfox@gmail.com>
2019-07-18 10:53:15 -0400

    Se agregar atributos IOS

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'imutable-v1';
 
