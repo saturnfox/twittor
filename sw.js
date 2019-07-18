5a910a4d6ac3c72142dcd2e804998951fc2c15ab
Erick Cares <saturnfox@gmail.com>
2019-07-18 11:38:13 -0400

    Modificaciones de iphone X (notch)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 importScripts('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'imutable-v1';
 
 const APP_SHELL = [
@@ -51,6 +51,12 @@ self.addEventListener('activate', e => {
             if (key !== STATIC_CACHE && key.includes('static')){
                 return caches.delete(key);
             }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')){
+                return caches.delete(key);
+            }
+
+
 
         });
 
